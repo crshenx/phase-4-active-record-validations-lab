676f6a63adc34d0e5dc9a436fdfe67cf605411eb
Chris Hendricks <crshenx@gmail.com>
2022-08-10 17:02:26 -0700

    commit tree 0b8ab705af0cfe01760b13e067f6a6ca042f0841

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validate :title_is_clickbait?, on: :create
+    validates :title, presence: true
+    validates :content, length:{minimum: 250}
+    validates :summary, length:{maximum: 250}
+    validates :category, inclusion: { in: ["Fiction","Non-Fiction"]}
+    # validates 
+
+    TITLE = [/Won't Believe/i, /Secret/i, /Top\d/i, /Guess/i]
+
+    def title_is_clickbait?
+        # validates :title, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"]}
+        if TITLE.none? {|w| w.match title}
+            errors.add(:title, "not clickbait")
+        end
+    end
 end
